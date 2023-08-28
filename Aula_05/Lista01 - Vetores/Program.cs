a267191e1abc63c60bc97f87bd903282e0445b36
Pedro Melo <pedromarcosprado26@gmail.com>
2023-08-28 15:36:35 -0300

    teste

---
+++ Aula_05/Lista01 - Vetores/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Lista01___Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // --- Exercício 01 ---
+            int[] vetor = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+            int indexNumero = -1, numeroDigitado;
+            string encontrado = "";
+
+
+            Console.Write("Digite o número que você deseja encontrar: ");
+            numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (vetor[i] == numeroDigitado)
+                {
+                    indexNumero = i;
+                    break;
+                }
+            }
+
+            encontrado = indexNumero < 0 ?
+                 $"O número {numeroDigitado} não foi encontrado!"
+                : $"O número {numeroDigitado} está localizado na posição: {indexNumero}. ";
+
+            Console.WriteLine(encontrado);
+
+
+
+            // --- Exercício 02 ---
+            int tamanhoVetor = 0;
+
+            Console.Write("Qual será o tamanho do vetor ? ");
+            tamanhoVetor = Convert.ToInt32(Console.ReadLine());
+
+
+            int[] vetor2 = new int[tamanhoVetor];
+
+            int ordemNumero = 1, indiceNumero = 0;
+            string elementosIndicesImpares = "", elementosPares = "";
+            double soma = 0, media;
+
+            do
+            {
+                Console.Write($"Digite o {ordemNumero}º número: ");
+                vetor2[indiceNumero] = Convert.ToInt32(Console.ReadLine());
+
+                indiceNumero++;
+                ordemNumero++;
+
+            } while (ordemNumero <= vetor2.Length);
+
+
+            for (int j = 0; j < tamanhoVetor; j++)
+            {
+                if (j % 2 == 1)
+                    elementosIndicesImpares += $"{vetor2[j]} ";
+
+                if (vetor2[j] % 2 == 0)
+                    elementosPares += $"{vetor2[j]} ";
+
+                soma += vetor2[j];
+            }
+
+            media = soma / tamanhoVetor;
+
+            Console.WriteLine($"Elementos nos índices ímpares: {elementosIndicesImpares} \r\nElementos pares: {elementosPares} \r\nSoma: {soma} \r\nMédia: {media} ");
+
+        }
+    }
+}
---
+++ Aula_05/Lista02 - Matrizes/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Lista02___Matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // --- Exercício 03 ---
+
+            int[,] matriz = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            string diagonalPrincipal = $"{matriz[0, 0]} {matriz[1, 1]} {matriz[2, 2]}";
+            string diagonalSecundaria = $"{matriz[0, 2]} {matriz[1, 1]} {matriz[2, 0]}";
+
+            int somaPrincipal = matriz[0, 0] + matriz[1, 1] + matriz[2, 2];
+            int somaSecundaria = matriz[0, 2] + matriz[1, 1] + matriz[2, 0];
+
+
+            Console.WriteLine($"Elementos da Diagonal Principal: {diagonalPrincipal} \r\nElementos da Diagonal Secundária: {diagonalSecundaria}");
+            Console.WriteLine($"Soma dos Elementos da Diagonal Principal: {somaPrincipal} \r\nSoma dos Elementos da Diagonal Segundária: {somaSecundaria}");
+
+
+
+            // --- Exercício 04 ---
+
+            int[,] matriz2 = new int[4, 10];
+
+            double mediaParticipante = 0;
+
+
+            foreach (int i in matriz2)
+            {
+
+            }
+
+
+
+
+
+        }
+    }
+}
