3f0204821223565b72820d4294a25eb33291a966
Pedro Melo <pedromarcosprado26@gmail.com>
2023-08-29 13:53:54 -0300

    Exercícios de estrutura de dados e collections resolvidos

---
+++ Aula_06/ListaExercicios_01_C#09/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace ListaExercicios_01_C_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> fila = new Queue<string>();
+
+            //1: Adicionar um novo Cliente na fila. Deve solicitar o nome do Cliente.
+            //2: Listar todos os Clientes na fila
+            //3: Chamar(retirar) uma pessoa da fila
+            //0: O programa deve ser finalizado.
+
+            string menu =
+                "\n1 - Adicionar cliente na fila" +
+                "\n2 - Listar todos os clientes" +
+                "\n3 - Retirar cliente da fila" +
+                "\n0 - Sair";
+
+            int numeroDigitado;
+            string? nome;
+
+
+            while (true)
+            {
+                Console.WriteLine(menu);
+                Console.Write("\nDigite a opção desejada: ");
+                numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+                if (numeroDigitado == 1)
+                {
+                    Console.Write("Digite seu nome: ");
+                    nome = Console.ReadLine()!;
+                    fila.Enqueue(nome);
+                    Console.Write("\nAdicionado à fila!\n ");
+                }
+
+                if (numeroDigitado == 2)
+                {
+                    if (fila.Count > 0)
+                    {
+                        Console.WriteLine($"\nFila:\n");
+
+                        foreach (var pessoa in fila)
+                        {
+                            Console.WriteLine(pessoa);
+                        }
+
+
+                    }
+                    else
+                        Console.WriteLine("\nA fila está vazia.\n");
+                }
+
+                if (numeroDigitado == 3)
+                {
+                    if (fila.Count() != 0)
+                    {
+                        Console.WriteLine($"\nPróximo da fila: {fila.Peek()}");
+                        fila.Dequeue();
+                        Console.WriteLine("O cliente foi chamado!\n");
+
+                    }
+
+                    else
+                        Console.WriteLine("\nA fila está vazia.\n");
+                }
+
+                if (numeroDigitado == 0)
+                    break;
+
+            }
+
+            Console.WriteLine("\n\nVolte sempre!\n\n");
+
+        }
+    }
+}
---
+++ Aula_06/ListaExercícios_02_C#09/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace ListaExercícios_02_C_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> pilha = new Stack<string>();
+
+            string menu =
+                "\n1 - Adicionar livro na pilha" +
+                "\n2 - Listar todos os livros" +
+                "\n3 - Retirar livro da pilha" +
+                "\n0 - Sair";
+
+            int numeroDigitado;
+            string? livro;
+            while (true)
+            {
+                Console.WriteLine(menu);
+                Console.Write("\nEntre com a opção desejada: ");
+                numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+                if (numeroDigitado == 1)
+                {
+                    Console.Write("\nDigite o nome do livro: ");
+                    livro = Console.ReadLine()!;
+                    pilha.Push(livro);
+
+                    Console.Write("Pilha: ");
+                    foreach (var i in pilha)
+                        Console.WriteLine(i);
+
+                    Console.Write("\nLivro adicionado!\n\n");
+                }
+
+
+                else if (numeroDigitado == 2)
+                {
+                    if (pilha.Count() > 0)
+                    {
+                        Console.WriteLine("\nLista de livros na pilha:\n");
+                        foreach (var i in pilha)
+                            Console.WriteLine(i);
+                    }
+                    else
+                        Console.WriteLine("\nA pilha está vazia. \n");
+                }
+
+
+                else if (numeroDigitado == 3)
+                {
+                    if (pilha.Count() > 0)
+                    {
+                        pilha.Pop();
+                        Console.WriteLine("\nPilha: \n");
+                        foreach (var i in pilha)
+                            Console.WriteLine(i);
+
+                        Console.WriteLine("\nUm livro foi retirado da pilha! \n");
+                    }
+                    else
+                        Console.WriteLine("\nA pilha está vazia. \n");
+                }
+
+                else if (numeroDigitado == 0)
+                    break;
+            }
+
+            Console.WriteLine("\n\nVolte sempre!\n\n");
+
+        }
+    }
+}
