d1ef371a421c5f9493b39013158a9d2276f0fc6c
Pedro Melo <pedromarcosprado26@gmail.com>
2023-09-01 10:56:36 -0300

    Aula 09 - Heranças

---
+++ Aula_09/Transportes/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Transportes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Transporte t1 = new Transporte(5);
+
+            t1.Visualizar();
+
+            Console.WriteLine();
+
+            Terrestre t2 = new Terrestre(4, 4, 100);
+
+            t2.Visualizar();
+
+            Automovel a1 = new Automovel(2, 2, "ABC1234", 200, "verde", 0, 5);
+
+            a1.Visualizar();
+
+
+        }
+    }
+}
---
+++ Aula_09/Transportes/Transporte.cs
@@ -0,0 +1,38 @@
+﻿namespace Transportes
+{
+    public class Transporte
+    {
+        private int capacidade;
+
+        public Transporte(int capacidade)
+        {
+            this.capacidade = capacidade;
+        }
+
+        public int GetCapacidade()
+        {
+            return this.capacidade;
+        }
+
+        public void SetCapacidade(int capacidade)
+        {
+            this.capacidade = capacidade;
+        }
+
+
+
+
+        public virtual void Visualizar()
+        {
+            Console.WriteLine("\n\n--------------------------------------");
+            Console.WriteLine("\t Meio de transporte");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"Capacidade: {this.capacidade}");
+
+            //Console.WriteLine($"Outras propriedades em override Terrestre.cs ");
+            //Console.WriteLine($"");
+            //Console.WriteLine($"");
+            //Console.WriteLine($"");
+        }
+    }
+}
